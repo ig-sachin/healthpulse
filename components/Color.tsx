f77e283b85253c49307468ebab9cdb25178d6116
Prajjwal <prajjwalfire@gmail.com>
2024-07-19 00:08:32 +0530

    added styling of color game

--- components/Color.tsx
+++ components/Color.tsx
@@ -9,6 +9,9 @@ const getRandomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
 const ColorGame = () => {
   const [colorName, setColorName] = useState('');
   const [fontColor, setFontColor] = useState('');
+  const [score, setScore] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(60);
+  const [gameOver, setGameOver] = useState(false);
 
   const generateNewColors = () => {
     const newColorName = getRandomElement(colors);
@@ -24,15 +27,58 @@ const ColorGame = () => {
     generateNewColors();
   }, []);
 
+  useEffect(() => {
+    if (timeLeft > 0 && !gameOver) {
+      const timer = setInterval(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000);
+      return () => clearInterval(timer);
+    } else if (timeLeft === 0) {
+      setGameOver(true);
+    }
+  }, [timeLeft, gameOver]);
+
   const handleNewColors = () => {
     generateNewColors();
   };
 
+  const handleColorButtonClick = (color) => {
+    if (color === fontColor) {
+      setScore(score + 1);
+    } else {
+      setScore(score - 1);
+    }
+    generateNewColors();
+  };
+
+  const handleRestartGame = () => {
+    setScore(0);
+    setTimeLeft(60);
+    setGameOver(false);
+    generateNewColors();
+  };
+
   return (
-    <div className="color-game border-orange-600">
+    <div className="color-game">
+      <div className="flex color-app-header">
+        <p className="temp">Time:{timeLeft}</p>
+        <p>Score:{score}</p>
+      </div>
       <h1 style={{ color: fontColor }}>{colorName}</h1>
-      <button onClick={handleNewColors}>New Colors</button>
-    </div>
+      <div className="buttons">
+        {colors.map((color) => (
+          <button
+            key={color}
+            style={{ backgroundColor: color.toLowerCase() }}
+            onClick={() => handleColorButtonClick(color)}
+            disabled={gameOver}
+          >
+            {color}
+          </button>
+        ))}
+      </div>
+      <button className="new-color-button" onClick={handleNewColors} disabled={gameOver}>New Colors</button>
+    </div >
   );
 };
 
