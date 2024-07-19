afb97d9e0d25f2445a7ae7294fcad3f797683d9c
ig-sachin <sachinvishwakarma7977@gmail.com>
2024-07-19 13:30:24 +0530

    Minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          "min-h-screen bg-dark-300 font-sans antialiased",
+          "min-h-screen bg-slate-800 font-sans antialiased",
           fontSans.variable
         )}
       >
