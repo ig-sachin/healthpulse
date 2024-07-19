4fc980c5fca58db4539e32cea531c7904d4c666b
ig-sachin <sachinvishwakarma7977@gmail.com>
2024-07-19 12:32:35 +0530

    .

--- components/Box.tsx
+++ components/Box.tsx
@@ -3,7 +3,6 @@
 // components/Box.tsx
 import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 interface BoxProps {
@@ -16,16 +15,17 @@ interface BoxProps {
 const Box: React.FC<BoxProps> = ({ imageUrl, altText, description, linkTo }) => {
   const router = useRouter();
   return (
-    // <Link href={linkTo} className="flex flex-col items-center p-4 rounded-lg shadow-md hover:shadow-lg transition duration-300">
     <div className="flex flex-col items-center p-4 rounded-lg shadow-md hover:shadow-lg transition duration-300">
-      <Image
-        src={imageUrl}
-        alt={altText}
-        width={200}
-        height={150}
-        className="rounded-md"
-      />
-
+      <div className="image-container">
+        <Image
+          src={imageUrl}
+          alt={altText}
+          width={200}
+          height={200}
+          style={{ maxHeight: 140 }}
+          className="object-cover rounded-md"
+        />
+      </div>
       <div className='flex flex-col justify-between'>
         <p className="mt-2 text-center text-white font-bold text-xl">{description}</p>
         <button onClick={() => router.push(linkTo)} className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -33,7 +33,6 @@ const Box: React.FC<BoxProps> = ({ imageUrl, altText, description, linkTo }) => 
         </button>
       </div>
     </div>
-
   );
 };
 
